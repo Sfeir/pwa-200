b2fef7020e9a6a51cb1565c63c44c2d5e53fd6b9
Noel Mace <contact@noelmace.com>
2018-08-28 02:21:21 +0200

    add people search feature in vanilla app

--- steps/step-vanilla/components/People.js
+++ steps/step-vanilla/components/People.js
@@ -1,6 +1,6 @@
-import { RoutedComponent } from '../../../common/utils/component/RoutedComponent'
-import { PlainComponent } from '../../../common/utils/component/PlainComponent'
-import { PeopleCardComponent } from './PeopleCard';
+import { RoutedComponent } from "../../../common/utils/component/RoutedComponent";
+import { PlainComponent } from "../../../common/utils/component/PlainComponent";
+import { PeopleCardComponent } from "./PeopleCard";
 
 export class PeopleComponent extends RoutedComponent {
   constructor(parent, { peoples }) {
@@ -8,14 +8,14 @@ export class PeopleComponent extends RoutedComponent {
     this.peoplesService = peoples;
     this.filteredPeople = [];
     this.loading = true;
-    this.query = '';
+    this.query = "";
   }
 
   async getPeoples() {
     this.loading = true;
     this.peoples = await this.peoplesService.getPeoples();
     this.loading = false;
-  };
+  }
 
   async init() {
     const container = document.createElement("div");
@@ -25,26 +25,35 @@ export class PeopleComponent extends RoutedComponent {
     await this.getPeoples();
   }
 
-  filterPeople(ids) {
-    this.filteredPeople = this.peoples.filter(people => ids.includes(''+people.id));
+  filterPeopleById(id) {
+    const people = this.peoples.find(people => id === ("" + people.id));
+    this.filteredPeople = people ? [ people ] : [];
+  }
+
+  filterPeopleByName(str) {
+    const query = str.toLowerCase();
+    this.filteredPeople = this.peoples.filter(people => {
+      const name = `${people.firstname} ${people.lastname}`.toLowerCase();
+      return name.includes(query);
+    });
   }
 
   // TODO: split between a ListComponent and a ShowPeopleComponent for better filtering management
-  async render([ peopleId ] = [], filter = true) {
+  async render([peopleId] = [], filter = true) {
     if (!this.container) {
       await this.init();
     }
 
     if (filter) {
       if (peopleId) {
-        this.filterPeople([ peopleId ]);
+        this.filterPeopleById(peopleId);
       }
     } else {
       this.filteredPeople = this.peoples;
     }
 
     // TODO: hidden optimization
-    if (this.filteredPeople.length < 1 && !this.loading) {
+    if (this.filteredPeople.length < 1) {
       this.content.render(`
         <div class="people-list-all md-padding" layout="row" layout-wrap layout-align="center center">
             <div>
@@ -53,21 +62,62 @@ export class PeopleComponent extends RoutedComponent {
                 </div>
             </div>
         </div>
-      `)
+      `);
     } else {
       this.content.render(`
-        <div class="people-list-all md-padding" layout="row" layout-wrap layout-align="center center">
-            <div class="people-card-list" data-people-cards-wrapper>
+        <div class="people-list-all md-padding" layout="row" layout-wrap layout-align="center center">`
+            +(!peopleId
+            ? `<form>
+              <div class="mdl-textfield mdl-js-textfield mdl-textfield--expandable">
+                <label class="mdl-button mdl-js-button mdl-button--icon" for="pleople-search">
+                  <i class="material-icons">search</i>
+                </label>
+                <div class="mdl-textfield__expandable-holder">
+                  <input class="mdl-textfield__input" type="text" id="pleople-search">
+                  <label class="mdl-textfield__label" for="sample-expandable">Expandable Input</label>
+                </div>
+              </div>
+            </form>` : '')+
+            `<div class="people-card-list" data-people-cards-wrapper>
             </div>
         </div>
-      `)
+      `);
 
-      // TODO: optimization for re-rendering
-      const peopleCardsWrapper = this.content.container.querySelector('[data-people-cards-wrapper]');
-      this.peopleCards = this.filteredPeople.map(people => ({ component: new PeopleCardComponent(peopleCardsWrapper), people }));
-      this.peopleCards.forEach(({ component, people}) => {
-        component.render({ people, describe: false })
-      });
+      const peopleCardsWrapper = this.content.container.querySelector(
+        "[data-people-cards-wrapper]"
+      );
+
+      const renderList = () => {
+        peopleCardsWrapper.innerHTML = '';
+        // TODO: optimization for re-rendering
+        this.peopleCards = this.filteredPeople.map(people => ({
+          component: new PeopleCardComponent(peopleCardsWrapper),
+          people
+        }));
+        this.peopleCards.forEach(({ component, people }) => {
+          component.render({ people, describe: false });
+        });
+      };
+
+      renderList();
+
+      if (!peopleId) {
+        // TODO: manager 'enter' key
+        this.content.container.querySelector("#pleople-search").addEventListener(
+          "keyup",
+          e => {
+            const query = e.srcElement.value;
+            if (query.length > 2) {
+              this.filterPeopleByName(query);
+            } else {
+              this.filteredPeople = this.peoples;
+            }
+            renderList();
+          },
+          true
+        );
+      }
+
     }
   }
 }
